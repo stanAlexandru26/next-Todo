d0dd10c9dfb64dbba262ec8bc8f60c52ff1aa716
stanAlexandru26 <stan.alexandru26@gmail.com>
2022-08-04 00:30:18 +0300

    fix: add seo in homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Layout from '@/components/layout/Layout';
+import Seo from '@/components/layout/Seo';
 
 export default function HomePage() {
   return (
     <Layout>
+      <Seo />
       <h1>hello</h1>
     </Layout>
   );
